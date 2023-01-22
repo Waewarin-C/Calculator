c9eba430468dd513772eb7b3b7f6f960dd88b51d
Waewarin Chindarassami <w.chindarassami@gmail.com>
2023-01-21 18:55:19 -0600

    Begin coding

--- CalculatorController.java
+++ CalculatorController.java
@@ -1,2 +1,67 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CalculatorController {
+    @FXML
+    private TextField numberDisplay;
+
+    public void showOnDisplay()
+    {
+
+    }
+
+    public void add()
+    {
+
+    }
+
+    public void subtract()
+    {
+
+    }
+
+    public void multiply()
+    {
+
+    }
+
+    public void divide()
+    {
+
+    }
+
+    public void convertToPercent()
+    {
+
+    }
+
+    public void plusMinus()
+    {
+
+    }
+
+    public void squareNumber()
+    {
+
+    }
+
+    public void squareRootNumber()
+    {
+
+    }
+
+    public void deleteANumber()
+    {
+
+    }
+
+    public void clearEntry()
+    {
+
+    }
+
+    public void clearAll()
+    {
+
+    }
 }
