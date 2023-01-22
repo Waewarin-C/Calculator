6237cff336ab989027b17f5730d5c4df22c3d0ea
Waewarin Chindarassami <w.chindarassami@gmail.com>
2023-01-21 19:03:56 -0600

    Continue working

--- Main.java
+++ Main.java
@@ -1,2 +1,32 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        stage = primaryStage;
+
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("./Calculator.fxml"));
+            stage.setScene(new Scene(root, 840, 640));
+            stage.show();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }
